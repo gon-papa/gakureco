fb4936843ef77b46b72d8618d6a074eaa6f7ae04
gonpapa <naoki.0821g@gmail.com>
2021-06-25 16:04:18 +0900

    バリデーション条件分岐追加

--- data/user.go
+++ data/user.go
@@ -16,19 +16,44 @@ type User struct {
 	Password   string `validate:"required,alphanum"`
 }
 
+func (u *User) ValidationCheck() (result map[string]string, err error) {
+	result = make(map[string]string)
+	err = validator.New().Struct(u)
+	if err != nil {
+		errors := err.(validator.ValidationErrors)
+		if len(errors) != 0 {
+			for i := range errors {
+				switch errors[i].StructField() {
+				case "Name":
+					result["Name"] = "名前を入力してください"
+					// fmt.Println(result["Name"])
+				case "Email":
+					result["Email"] = "正しいメールアドレスの形式で入力してください"
+					// fmt.Println(result["Email"])
+				case "Password":
+					result["Password"] = "パスワードを入力してください"
+					// fmt.Println(result["Password"])
+				}
+			}
+		}
+		return result, err
+	}
+	return result, err
+}
+
 func (u *User) CreateUser(r *http.Request) {
-	user := User{
+	user := &User{
 		Name:     r.FormValue("name"),
 		Email:    r.FormValue("email"),
 		Password: r.FormValue("password"),
 	}
 
-	validate_err := validator.New().Struct(user)
-	if validate_err != nil {
-		//後にvalidetioncheck関数からtemplateに返す
-		fmt.Println(validate_err, "validationにひっかかりました")
+	s, err := user.ValidationCheck()
+	if err != nil {
+		fmt.Println(s, err)
 		return
 	}
+
 	if err := Db.Create(&user).Error; err != nil {
 		fmt.Println(err)
 		return
